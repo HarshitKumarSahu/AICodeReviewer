7694c3ee6be06c59c507ed6c18ea3dd6921fb4ce
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-04-19 11:35:06 +0530

    crated basic ai intragitation and setup reactapp

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const aiRouter = require("./routes/ai.routes")
 
 const app = express();
 
+// used for post request in /getReview
+app.use(express.json())
+
+//index route or home route
 app.get("/", (req,res)=>{
     res.send("test route")
 })
+
+app.use("/ai", aiRouter)
 
 module.exports = app;
